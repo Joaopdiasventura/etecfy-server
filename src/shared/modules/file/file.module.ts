34d71aeb86cebd4db8c5e0ab076a2c84e99ff9cb
João Paulo Dias <joaopdias.dev@gmail.com>
2025-12-29 16:02:02 -0300

    fix: using the aws

--- src/shared/modules/file/file.module.ts
+++ src/shared/modules/file/file.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FileService } from './file.service';
 import { LocalService } from './providers/local/local.service';
+import { AwsService } from './providers/aws/aws.service';
 
 @Module({
   imports: [ConfigModule],
   providers: [
     FileService,
     { provide: 'DevelopmentStorage', useClass: LocalService },
-    { provide: 'ProductionStorage', useClass: LocalService },
+    { provide: 'ProductionStorage', useClass: AwsService },
   ],
   exports: [FileService],
 })
